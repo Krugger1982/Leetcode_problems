ed03f838749c69a799115154450e4aaeb9633878
Alexey Razumovskiy <razumyanchik@mail.ru>
2021-10-04 11:27:52 +0300

    Task 27 Podstroka

--- Task27_strStr.py
+++ Task27_strStr.py
@@ -6,12 +6,7 @@ class Solution:
             if haystack[index] != needle[0]:
                 continue
             else:
-                Substr = True
-                for i in range(len(needle)):
-                    if haystack[index + i] != needle[i]:
-                       Substr = False
-                       break
-                if Substr:
+                if haystack[index:index+len(needle)] == needle:
                     return index
             
         return -1    
@@ -48,13 +43,20 @@ class MyTests(unittest.TestCase):
 
     def test_4(self):
         haystack = 'hello'*1000000 + 'helloga' + 'hello'*100
-#        print(haystack)
         needle = 'helloga'
         out = 5000000
         answer = Solution()
         k = answer.strStr(haystack, needle)
         self.assertEqual(out, k)
-            
+
+    def test_5(self):
+        haystack = 'a'
+        needle = 'a'
+        out = 0
+        answer = Solution()
+        k = answer.strStr(haystack, needle)
+        self.assertEqual(out, k)
+        
 if __name__ == '__main__':
     try: unittest.main()
     except SystemExit: pass
